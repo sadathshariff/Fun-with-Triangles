f56429b88e5ed6f7e7139fb956aadeaa23d737fe
sadath <sadathshariff65@gmail.com>
2021-08-24 15:58:04 +0530

    completed Triangle Quiz

---
+++ quiz.js
@@ -0,0 +1,22 @@
+const quizForm = document.querySelector(".quiz-form");
+const submitAnswerBtn = document.querySelector("#submit-answer-btn");
+
+const outputDiv = document.querySelector(".output-div");
+
+const correctAnswers = ["90°", "Right angled"];
+
+function calculateScore() {
+  let score = 0;
+  let index = 0;
+  const formResults = new FormData(quizForm);
+  for (let value of formResults.values()) {
+    if (value === correctAnswers[index]) {
+      score = score + 1;
+    }
+    index = index + 1;
+  }
+  console.log(score);
+  outputDiv.innerText = `Your Score is ${score}`;
+}
+
+submitAnswerBtn.addEventListener("click", calculateScore);
