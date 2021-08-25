bd98586a0bd4e11517f289b19744487a858b9707
sadath <sadathshariff65@gmail.com>
2021-08-25 13:20:46 +0530

    Added Exception Handling

--- IsTriangle/index.js
+++ IsTriangle/index.js
@@ -2,8 +2,13 @@ const inputs = document.querySelectorAll(".input-angle");
 const isTriangleBtn = document.querySelector("#is-triangle-btn");
 const outputDiv = document.querySelector("#output");
 
+function showMessage(msg) {
+  outputDiv.innerText = msg;
+}
+
 function calculateSumOfAngles(angle1, angle2, angle3) {
   const sumOfAngles = angle1 + angle2 + angle3;
+  validate(angle1, angle2, angle3);
   return sumOfAngles;
 }
 
@@ -13,10 +18,21 @@ function isTriangle() {
     Number(inputs[1].value),
     Number(inputs[2].value)
   );
+
   if (sumOfAngles === 180) {
-    outputDiv.innerText = "Yayy Angles formed a Triangle";
+    showMessage("Yayy Angles formed a Triangle");
   } else {
-    outputDiv.innerText = "Ohh Angles don't form a Triangle";
+    showMessage("Ohh, These Angles don't form a Triangle");
+  }
+}
+
+function validate(angle1, angle2, angle3) {
+  if (angle1 == "") {
+    alert("Please Fill All the Fields");
+  } else if (angle2 == "") {
+    alert("please fill all the fields");
+  } else if (angle3 == "") {
+    alert("please Fill all the fields");
   }
 }
 
