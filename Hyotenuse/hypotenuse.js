43418bdc7e4da1adb6f97d5a414c6cd58f6fedf6
sadath <sadathshariff65@gmail.com>
2021-08-25 16:08:28 +0530

    App Completed

--- Hyotenuse/hypotenuse.js
+++ Hyotenuse/hypotenuse.js
@@ -4,6 +4,7 @@ const outputDiv = document.querySelector(".output-div");
 
 function calculateSumOfSquares(a, b) {
   const sumOfSquares = a * a + b * b;
+
   return sumOfSquares;
 }
 
@@ -12,9 +13,19 @@ function calculateHypotenuse() {
     Number(sideInputs[0].value),
     Number(sideInputs[1].value)
   );
+  validate(sideInputs[0].value, sideInputs[1].value);
+
   const lengthOfHypotenuse = Math.sqrt(sumOfSquares);
   const roundedValue = lengthOfHypotenuse.toPrecision(4);
   outputDiv.innerText = `The length of Hypotenuse is ${roundedValue}`;
+}
+
+function validate(a, b) {
+  if (a == "") {
+    alert("Enter both fields to calulate");
+  } else if (b == "") {
+    alert("Enter both fields to calulate");
+  }
 }
 
 hypotenuseBtn.addEventListener("click", calculateHypotenuse);
