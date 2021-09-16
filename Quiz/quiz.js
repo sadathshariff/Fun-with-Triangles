e70c7028bcb3e94a216c0f01810bd472e76471f1
sadath <sadathshariff65@gmail.com>
2021-09-16 12:49:07 +0530

    Added more Questions to quiz

--- Quiz/quiz.js
+++ Quiz/quiz.js
@@ -3,12 +3,23 @@ const submitAnswerBtn = document.querySelector("#submit-answer-btn");
 
 const outputDiv = document.querySelector(".output-div");
 
-const correctAnswers = ["90°", "Right angled"];
+const correctAnswers = [
+  "90°",
+  "Right angled",
+  "Equilateral",
+  "a+b+c",
+  "one right angle",
+  "28",
+  "180",
+  "hypotenuse",
+  "5",
+  "Isosceles",
+];
 
 function calculateScore() {
   let score = 0;
   let index = 0;
-  const formResults = new FormData(quizForm);
+  let formResults = new FormData(quizForm);
 
   for (let value of formResults.values()) {
     if (value === correctAnswers[index]) {
